4252315990c934cc625f3e16ce64758318a663cd
suneettipirneni <suneettipirneni@icloud.com>
2021-08-25 14:38:46 -0400

    add initial logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-(function main() {
-  console.log('Hello World!');
+import dotenv from 'dotenv';
+import { Client } from '@knighthacks/dispatch';
+
+// Load env vars in
+dotenv.config();
+
+(async function main() {
+  const client = new Client({
+    intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_PRESENCES', 'GUILD_MEMBERS'],
+  });
+
+  await client.login(process.env.DISCORD_TOKEN);
 })();
